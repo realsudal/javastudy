f2502a62979df1a2ba18d35a30434078677d8a29
realsudal <pnjpass@naver.com>
2021-03-30 17:17:45 +0900

    update 2021-03-30

--- workspace/14_network/src/ex02_socket/ClientMain.java
+++ workspace/14_network/src/ex02_socket/ClientMain.java
@@ -1,16 +1,19 @@
 package ex02_socket;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+
+
 
 public class ClientMain {
 
 	public static void main(String[] args) {
 
 		
-		//Socket : 클라이언트  - 	먼저 실행
+		//Socket : 클라이언트  - 	나중에 실행 + 계속 재실행해도됨.
 		
 		
 		Socket client=null;
@@ -27,6 +30,13 @@ public class ClientMain {
 			//접속성공
 			System.out.println("접속 성공");
 			
+			//server로부터 메시지 받기
+			InputStream is=client.getInputStream();
+			byte[] b=new byte[50];
+			int length=is.read(b);//읽은 내용은 b에 저장, 읽은 바이트 수(글자수)는 length에 저장
+			// new string() 메서드 : byte[] ->string
+			String msg=new String(b, 0, length, "UTF-8");
+			System.out.println(msg);
 			
 			
 			
--- workspace/14_network/src/ex02_socket/ServerMain.java
+++ workspace/14_network/src/ex02_socket/ServerMain.java
@@ -1,5 +1,7 @@
 package ex02_socket;
 
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,17 +11,18 @@ public class ServerMain {
 	public static void main(String[] args) {
 
 		
-		//ServerSocket : 서버
+		//ServerSocket : 서버	-	 딱 1번만 실행
 		
 		ServerSocket server=null;
 		
 		try {
+			
+			
 			//서버 생성
 			server=new ServerSocket();
 			System.out.println("클라이언트가 서버에 접속을 시도합니다.");
 			//접속할 클라이언트 정보
 			server.bind(new  InetSocketAddress("localhost",4966 ));
-			
 			//서버는 항상 켜져있다. (=무한루프)
 			while(true) {
 				System.out.println("서버 실행중입니다.");
@@ -29,6 +32,14 @@ public class ServerMain {
 				//클라이언트의 주소
 				InetSocketAddress isa = (InetSocketAddress) client.getRemoteSocketAddress();
 				System.out.println("[ "+isa.getHostName()+" ] 가 접속되었습니다.");
+				
+				//클라이언트에게 웰컴메시지를 보내기
+				String msg="Welcome to server!";
+				//클라이언트에게 보낼수있는 스트림 생성
+				OutputStream os=client.getOutputStream();
+				os.write(msg.getBytes("UTF-8"));//getbytes() : string -> byte로 변환 메서드!!!!!
+				
+				
 			}
 			
 		}catch (Exception e) {
